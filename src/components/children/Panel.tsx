1cfc395b5348ca0c7829e220ba1778930e36d996
DannyRodriguezFSWD <dandemslay@hotmail.com>
2025-01-04 21:39:02 +0900

    zustand focus

--- src/components/children/Panel.tsx
+++ src/components/children/Panel.tsx
@@ -1,14 +1,33 @@
+import { useEffect, useState } from "react";
+
+
 import StepperIndicator from "./StepperIndicator";
 
+function getStepContent(step: number) {
+    switch (step) {
+      case 1:
+        return <div>trew1</div>;
+      case 2:
+        return <div>trew2</div>;
+      case 3:
+        return <div>trew3</div>;
+      case 4:
+        return <div>trew4</div>;
+     
+      default:
+        return "Unknown step";
+    }
+  }
 
 
 export const Panel = () => {
-
+    const [activeStep, setActiveStep] = useState(1);
     return (
         <div>
             <h1>Run Memories</h1>
             <hr></hr>
-            <StepperIndicator activeStep={5}/>
+            <StepperIndicator activeStep={activeStep}/>
+            {getStepContent(activeStep)}
 
         </div>
 
